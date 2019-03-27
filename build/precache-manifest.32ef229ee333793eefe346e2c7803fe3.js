c394aec50e3a62f2b35305e1fc7b1d2f03cbb87d
pavladan <44526604+pavladan@users.noreply.github.com>
2019-03-27 23:24:45 +0300

    upload build

---
+++ build/precache-manifest.32ef229ee333793eefe346e2c7803fe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f22dca1907514dcc3e34",
+    "url": "/static/css/main.f918c00e.chunk.css"
+  },
+  {
+    "revision": "f22dca1907514dcc3e34",
+    "url": "/static/js/main.f22dca19.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "6be924c68b4ed87e66e0",
+    "url": "/static/js/2.6be924c6.chunk.js"
+  },
+  {
+    "revision": "1855bb8e014b4e18f209fafe23d6cd9b",
+    "url": "/static/media/airplane.1855bb8e.svg"
+  },
+  {
+    "revision": "1cdc0d3390e9012be1781135ea8d110c",
+    "url": "/index.html"
+  }
+];
