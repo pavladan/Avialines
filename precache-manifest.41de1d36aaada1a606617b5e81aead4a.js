8506d2df7dbc844f426c6e901c72fee6de192e65
pavladan <44526604+pavladan@users.noreply.github.com>
2019-04-02 20:10:05 +0300

    Updates

---
+++ precache-manifest.41de1d36aaada1a606617b5e81aead4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6854bdf497519bb4643",
+    "url": "/Avialines/static/css/main.5bc4b2b1.chunk.css"
+  },
+  {
+    "revision": "a6854bdf497519bb4643",
+    "url": "/Avialines/static/js/main.a6854bdf.chunk.js"
+  },
+  {
+    "revision": "8bc133d14bec7fc45382",
+    "url": "/Avialines/static/js/runtime~main.8bc133d1.js"
+  },
+  {
+    "revision": "6be924c68b4ed87e66e0",
+    "url": "/Avialines/static/js/2.6be924c6.chunk.js"
+  },
+  {
+    "revision": "1855bb8e014b4e18f209fafe23d6cd9b",
+    "url": "/Avialines/static/media/airplane.1855bb8e.svg"
+  },
+  {
+    "revision": "6a3f99481d3e5a3bee6157bcedaf871b",
+    "url": "/Avialines/index.html"
+  }
+];
